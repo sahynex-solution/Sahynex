b172e8e9a99de478cbdcff601105fba08d0b4857
Skanda Ganesh P V <49191239+Skandaganesh@users.noreply.github.com>
2025-08-01 00:02:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Remove experimental.runtime - no longer needed in Next.js 15
-  // output: process.env.CF_PAGES ? 'export' : undefined,
-  output: 'export',
-  images: {
-    unoptimized: true
-  }
-  // Add if using Turbopack
-  // experimental: process.env.TURBO ? { turbo: {} } : {},
+ 
 };
-
-module.exports = nextConfig;
+export default nextConfig;
