b84ebce8fecf2946415416c49e065d0867ad6ed9
Skanda Ganesh P V <skandaganeshpv@gmail.com>
2025-08-01 20:48:59 +0530

    refactor:api renamed and route redefined

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -39,7 +39,7 @@ const ContactForm: FC = () => {
     };
 
     try {
-      const response = await fetch("/apis/contact", {
+      const response = await fetch("/api/contact", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(contactData),
