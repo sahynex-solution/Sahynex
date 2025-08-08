1ffedb1a028fe65336ef60204569462f17d4d44a
Skanda Ganesh P V <skandaganeshpv@gmail.com>
2025-08-09 00:46:07 +0530

    feat:Client onclick and optimization

--- src/Components/ClientSlider2.tsx
+++ src/Components/ClientSlider2.tsx
@@ -1,27 +1,37 @@
 "use client"
 
+import { link } from "fs"
 import Image from "next/image"
 
 const clients = [
   {
-    name: "Sahyadri College of Engineering and Management",
-    src: "/images/clients/Sayhadri.jpg",
+    name: (
+    <>
+      Sahyadri College Of Engineering<br /> And Management, Mangaluru
+    </>
+  ),
+    src: "/images/clients/Sahyadri.jpg",
+    link: "https://sahyadri.edu.in/"
   },
   {
     name: "Heggade Vahini",
     src: "/images/clients/heggade_vahini.webp",
+    link: "https://heggadevahini.com/"
   },
   {
     name: "Shri Rama Temple, Chokkadi",
     src: "/images/clients/mahesh_bhat_choontaru.jpg",
+    link: "https://srtchokkadi.org/"
   },
   {
     name: "Shri Harihareshwara Temple",
-    src: "/images/clients/kishor_kumar_kujugodu.jpg"
+    src: "/images/clients/kishor_kumar_kujugodu.jpg",
+    link: "https://shriharihareshwara.org/"
   },
   {
     name: "Coding Key LLP",
-    src: "/images/clients/cod.png"
+    src: "/images/clients/cod.png",
+    link: "https://www.codingkeyllp.com/"
   }
 //   ,{
 //     name: "Kulkunda Shri Basaveshwara Temple",
@@ -33,10 +43,10 @@ export default function ClientSlider() {
   return (
     <div className="w-full overflow-hidden py-12 bg-gradient-to-r from-[#f8f9ff] to-[#fdeef3]">
       <div className="relative overflow-hidden">
-        <h2 className="text-4xl md:text-5xl font-bold text-[#0e1f51] mt-2 mb-8">
+        <h2 className="text-4xl md:text-5xl font-bold text-[#0e1f51] mt-2 mb-12">
             <span className="text-[#e80312]">Our Clients</span>
         </h2>
-        <div className="flex animate-slide-fast md:animate-slide-medium whitespace-nowrap min-w-fit will-change-transform">
+        <div className="flex animate-slide-medium md:animate-slide-medium whitespace-nowrap min-w-fit will-change-transform">
   {[...clients, ...clients].map((client, index) => (
     <div
       key={`logo-${index}`}
@@ -51,12 +61,14 @@ export default function ClientSlider() {
             src={client.src}
             alt={`${client.name} logo`}
             fill
-            className="object-contain rounded-lg"
+            onClick={() => window.open(client.link, "_blank")}
+            className="object-contain rounded-lg hover:cursor-pointer"
             />
         </div>
-        <p className="text-[#1e293b] font-semibold text-base lg:text-lg leading-snug text-center !justify-center mt-4 whitespace-normal tracking-tight">
-          {client.name}
-        </p>
+       <p className="text-[#1e293b] font-semibold text-base lg:text-lg leading-snug text-center mt-4 tracking-tight">
+  {client.name}
+</p>
+
     </div>
     </div>
   ))}
