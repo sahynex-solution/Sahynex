69d2eeb8c71f8ca1cb37424ee1cb02d7cf81eee9
TarunG8504 <tarun.g8504@gmail.com>
2025-08-09 08:44:41 +0530

    feat(fix): name fix

--- src/Components/ClientSlider2.tsx
+++ src/Components/ClientSlider2.tsx
@@ -11,7 +11,7 @@ const clients = [
   {
     name: (
       <>
-        Sahyadri College of Engineering<br />and Management, Mangaluru
+        Sahyadri College of Engineering<br />&amp; Management, Mangaluru
       </>
     ),
     src: "/images/clients/Sahyadri.jpg",
